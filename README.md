0d70bf64be62f9182ad541c82a76032918027de0
xoxoajckie <jackiecampbell3@gmail.com>
2022-09-08 19:46:52 +0200

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# HEAVY work in progress
+
+## TODO:
+[x] basic deluge-execute script
+[] add logging to deluge-execute script
+[] create lftp config template
+[] fill in lftp config
+[] basic lftp script
+[] don't forget a trap (not everyone loves systemd)
+[] decide on logging in lftp script or leaving it up to the user
+
+### Deluge Settings
+
+Download Clients > Remote Path Mappings > `/home/thewiz/complete` -> `/Incoming/Sonarr`
+
+
+
---
+++ deluge-hardlink.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+##############################################################
+# SEE THE README FOR IMPORTANT INSTRUCTIONS
+# BEFORE SETTING THESE OPTIONS:
+outgoing_dir="$HOME/outgoing"
+labels_to_process=("sonarr" "sonarr-4k" "radarr" "radarr-4k")
+tmp_dir_labels=("sonarr" "sonarr-4k")
+label_conf_location="$HOME/.config/deluge/label.conf"
+##############################################################
+
+set -x
+
+torrent_id=$1
+torrent_name=$2
+torrent_path=$3
+
+torrent=$torrent_path/$torrent_name
+
+get-label () {
+    label=$(grep $torrent_id $label_conf_location | awk -F '"' '{print $4}')
+}
+
+check-label () {
+    if printf '%s\n' "${labels_to_process[@]}" | grep -q -P ^$label$; then
+        link_torrent="true"
+    else
+        link_torrent="false"
+    fi
+}
+
+set-link-dir () {
+    link_dir=${label^}
+    if printf '%s\n' "${tmp_dir_labels[@]}" | grep -q -P ^$label$; then
+        if [[ -d $torrent ]]; then
+            link_dir=$link_dir-tmp
+        fi
+    fi
+}
+
+make-the-links () {
+    mkdir -p "$outgoing_dir/$link_dir"
+    if [[ -d $torrent ]]; then
+        cp -rl "$torrent" "$outgoing_dir/$link_dir"
+    else
+        ln "$torrent" "$outgoing_dir/$link_dir"
+    fi
+}
+
+get-label
+check-label
+[[ $link_torrent == "false" ]] && exit 125
+set-link-dir
+make-the-links
+exit 0
