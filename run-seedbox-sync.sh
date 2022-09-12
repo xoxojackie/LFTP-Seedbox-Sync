aa8d391a3b5d52ef825133d1e3f01692c2c1711f
Jackie Campbell <jackiecampbell3@gmail.com>
2022-09-12 16:44:44 +0100

    add move-tmp-dirs function

--- run-seedbox-sync.sh
+++ run-seedbox-sync.sh
@@ -19,12 +19,20 @@ create-lock-file () {
     fi
 }
 
+move-tmp-dirs () {
+    find "$incoming_dir" -name "*-tmp" | while read tmpdir; do
+        non_tmpdir=${tmpdir::-4}
+        mv "$tmpdir"/* $non_tmpdir
+        [[ -z $(ls -A "$tmpdir") ]] && rm -r "$tmpdir"
+    done
+}
 
 cleanup () {
     [[ -e "$lockfile" ]] && rm "$lockfile"
 }
 
+trap cleanup SIGINT SIGTERM
 
 create-lock-file
-
+move-tmp-dirs
 cleanup
