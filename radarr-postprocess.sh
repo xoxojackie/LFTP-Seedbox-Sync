0378b07fac2cde7f512a3f77537bbb03106c65f2
Jackie Campbell <jackiecampbell3@gmail.com>
2022-09-14 15:45:18 +0100

    fix radarr-postprocess references to sonarr

--- radarr-postprocess.sh
+++ radarr-postprocess.sh
@@ -2,16 +2,16 @@
 
 [[ $radarr_eventtype == "Test" ]] && exit 0
 
-# removes the episode file
-[[ -e "$sonarr_episodefile_sourcepath" ]] && rm "$sonarr_episodefile_sourcepath"
+# removes the movie file
+[[ -e "$radarr_moviefile_sourcepath" ]] && rm "$radarr_moviefile_sourcepath"
 
-# removes the directory, if the directory is not named *Sonarr*
+# removes the directory, if the directory is not named *radarr*
 # and it is empty
-if [[ "$sonarr_episodefile_sourcefolder" =~ "sonarr" ]]; then
+if [[ "$radarr_moviefile_sourcefolder" =~ "radarr" ]]; then
     exit 0
 elif
-    [[ -z $(ls -A "$sonarr_episodefile_sourcefolder") ]]; then
-        rm -r "$sonarr_episodefile_sourcefolder"
+    [[ -z $(ls -A "$radarr_moviefile_sourcefolder") ]]; then
+        rm -r "$radarr_moviefile_sourcefolder"
 fi
 
 exit 0
